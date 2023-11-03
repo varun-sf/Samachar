42090436e2b13092e25cc6d65386222d16a8ed91
Varun <tilkireddy@gmail.com>
2023-11-03 12:36:27 +0530

    till infinite scroll, has a bug

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,8 +3,8 @@ import NewsItem from './NewsItem'
 
 import PropTypes from 'prop-types'
 import Loading from './Loading'
-import InfiniteScroll from 'react-infinite-scroller';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 
@@ -15,68 +15,77 @@ import InfiniteScroll from 'react-infinite-scroller';
 export default class News extends Component {
   static defaultProps={
     country: "in",
-    pageSize: 6
+    pageSize: 6,
+    category: 'general'
   }
 
   static propTypes={
     country: PropTypes.string,
-    pageSize: PropTypes.number
+    pageSize: PropTypes.number,
+    category: PropTypes.string
   }
 
   constructor(props){
     super(props);
     this.state ={
-      articles : this.articles,
+      articles : [],
       loading: false,
       page: 1,
       TotalPages:1,
       totalResults: 0
     }
+    // console.log(this.state.articles.length);
+   
     document.title = `Samachar-${this.props.category}`
   };
 
   
 
   async componentDidMount(){
-    // console.log("cdn");
+
+    this.setState({loading: true});
      let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ddb75b7372284aa598e91063139d06b5&pageSize=${this.props.pageSize}`);
   
      let parseddata = await data.json();
-    
-     this.setState({articles: parseddata.articles, TotalPages:Math.ceil(parseddata.totalResults/this.props.pageSize)});
+     
+     this.setState({articles: parseddata.articles, TotalPages:Math.ceil(parseddata.totalResults/this.props.pageSize), totalResults: parseddata.totalResults, loading: false});
+    //  console.log(this.state.articles.length)
   }
 
-   handleNextPage = async ()=>{
-    this.setState({loading: true});
-    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ddb75b7372284aa598e91063139d06b5&page=${this.state.page+1}&pageSize=${this.props.pageSize}`);
+  loadMore = async()=>{
+    this.setState({ page:this.state.page+1});
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ddb75b7372284aa598e91063139d06b5&page=${this.state.page}&pageSize=${this.props.pageSize}`);
     let parseddata = await data.json();
-    this.setState({articles: parseddata.articles,page:this.state.page+1,loading:false})
-  }
+    this.setState({articles: this.state.articles.concat(parseddata.articles)});
 
-  handlePreviousPage = async ()=>{
-    this.setState({loading: true});
-    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ddb75b7372284aa598e91063139d06b5&page=${this.state.page-1}&pageSize=${this.props.pageSize}`);
-    let parseddata = await data.json();
-    this.setState({articles: parseddata.articles,page:this.state.page-1,loading:false})
   }
   
   render() {
+
     return (
       <div className="container my-3">
         <h1 className='text-center' style={{margin: '20px 0px'}}>Samachar- Top headlines</h1>
-       {this.state.loading&& <Loading/>}
+        {this.state.loading && <Loading/>}
+      <InfiniteScroll
+         dataLength ={this.state.articles.length}
+          next={this.loadMore}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+        loader={<Loading/>}>
+      
+       <div className='container'>
         <div className='row'>
-        {this.state.articles?.map((element)=>                 
-                <div className='col-md-4 my-3' key ={element.url}>
-                  <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} author={element.author?element.author:"Unknown"} time={new Date(element.publishedAt).toGMTString()} imageUrl={element.urlToImage?element.urlToImage:"https://www.solodev.com/core/fileparse.php/131/urlt/Solodev_Blog_NewsAlerts_951x651.jpg"} newsurl={element.url} />
+        {this.state.articles.map((element)=>{
+                         
+              return <div className='col-md-4 my-3' key ={element.url} >
+                  <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} author={element.author?element.author:"Unknown"} time={new Date(element.publishedAt).toGMTString()} imageUrl={element.urlToImage?element.urlToImage:"https://www.solodev.com/core/fileparse.php/131/urlt/Solodev_Blog_NewsAlerts_951x651.jpg"} newsurl={element.url} />
                 </div>
-        )} 
+         } )}
          </div>
-        <div className='container d-flex justify-content-center'>
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark mx-2" onClick={this.handlePreviousPage}>Previous</button>
-        <button disabled={this.state.page>=this.state.TotalPages}type="button" className="btn btn-dark" onClick={this.handleNextPage}>Next</button>
-        </div>
+         </div>
+        </InfiniteScroll>
+
       </div>
     )
   }
 }
+
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -10,7 +10,7 @@ export default class NewsItem extends Component {
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
     <p className="card-text"><small className="text-body-secondary">By {author} on {time}</small></p>
-    <a href={newsurl} rel="noreferrer" target="_blank" className="btn btn-primary">Go somewhere</a>
+    <a href={newsurl} rel="noreferrer" target="_blank" className="btn btn-primary">Read more</a>
   </div>
 </div>
     )
